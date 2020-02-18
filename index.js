6d15a85c75c42e0c56c92be3eac23642530ddd80
Ryan K <ryankustas@gmail.com>
2020-02-18 11:42:56 -0700

    Correcting api

--- index.js
+++ index.js
@@ -3,15 +3,17 @@ var healthcare = "/US_healthcare_cost";
 var food = "/fooddata/";
 var countrynames = "/countrynames";
 var metrics = "/foodindex";
-var starting_country = "albania";
+var starting_country = "USA, Puerto Rico and US Virgin Islands";
 
 
-
+// function updateChart() {
 // This function is called when a dropdown menu item is selected
 d3.json(baseurl + food + starting_country, function(data) {
-    console.log(baseurl+food+starting_country);
+    // console.log(baseurl+food+starting_country);
     console.log(data);
 });
+
+// d3.selectAll("body").on("change", updateChart);
 
 
 
